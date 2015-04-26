d6943452e12383efdcbb1aa6bd013de4ec2c69e7
riconroy <riconroy@gmail.com>
2015-04-26 16:02:07 -0400

    Making a commit before cleaning things up; the two backgrounds move up together, and the blur gradually comes in.

--- weather/MainViewController.swift
+++ weather/MainViewController.swift
@@ -18,26 +18,29 @@ class MainViewController: UIViewController {
 		super.viewDidLoad()
 		
 		// we start with an unblurred image
-		let bgImage = UIImage(named: "torontoTest1.png")
+		let bgImage = UIImage(named: "imageTest1")
 		bgImageView = UIImageView(image: bgImage!)
-		bgImageView!.frame = view.frame
+		bgImageView?.contentMode = .Top
+		// bgImageView!.frame = view.frame
 		view.addSubview(bgImageView!)
 		
-		// create the same image on top, but blurred
-		let blurredImage = bgImage!.applyBlurWithRadius(30.0, tintColor: UIColor(white: 0.8, alpha: 0.73), saturationDeltaFactor: 1.8, maskImage: bgImage)
-		// let blurredImage = bgImage!.applyLightEffect()
 		
 		// create the same image on top, but blurred
-		// let blurredImage = bgImage?.applyBlurWithRadius(15.0, tintColor: UIColor(white: 1.0, alpha: 0.2),
-			// saturationDeltaFactor: 1.2, maskImage: bgImage)
+		// let blurredImage = bgImage!.applyBlurWithRadius(30.0, tintColor: UIColor(white: 0.8, alpha: 0.73), saturationDeltaFactor: 1.8, maskImage: bgImage)
+		// let blurredImage = bgImage!.applyLightEffect()
+		let reducedImage = resizeImageToNonRetina(bgImage!)
+		let blurredImage = applyBlurEffectToImage(reducedImage)
+		println("\(bgImage)")
+		println("\(reducedImage)")
+		println("\(blurredImage)")
 		
 		bgBlurImageView = UIImageView(image: blurredImage)
-		bgBlurImageView!.frame = view.frame
-		bgBlurImageView!.alpha = 0.0
+		bgBlurImageView?.contentMode = .Top
+		// bgBlurImageView!.frame = view.frame
 		view.addSubview(bgBlurImageView!)
 		
-		// math for laying out the grid
-		// println("width of screen = \(self.view.frame.size.width)")
+		// hide it
+		bgBlurImageView!.alpha = 0.0
 		
 		let myLayout = RCFlowLayout()
 		myLayout.setLayoutVariables(Int(self.view.frame.size.width))
@@ -59,16 +62,61 @@ class MainViewController: UIViewController {
 	}
 	
 	func scrollViewDidScroll(scrollView: UIScrollView) {
-		// println("scrolling = \(scrollView.contentOffset)")
+		// how far has the scroll been scrolled
 		var offsetY = scrollView.contentOffset.y
-		let maxOffset:CGFloat = 450.0
-		offsetY = min(maxOffset, max(0.0, offsetY))
-		// println("offsetY: \(offsetY)")
-		let thePercent = offsetY / maxOffset
-		// println("percent: \(thePercent)")
+		
+		// how far do we want the user to scroll to have full blur?
+		let maxOffset:CGFloat = scrollView.frame.size.height - 100.0
+		let scrollBlur = min(maxOffset, max(0.0, offsetY))
+		
+		// make that into a number [0, 1], and adjust the alpha of the (top) blurred image accordingly
+		let thePercent = scrollBlur / maxOffset
 		bgBlurImageView!.alpha = thePercent
+		
+		// how far up do we want the background image to rise?
+		let maxRise:CGFloat = 30.0
+		let riseRatio: CGFloat = 15.0
+		
+		// make that into an amount to rise the background image
+		let scrollBgMove = min((maxRise * riseRatio), max(0.0, offsetY))
+		let newFrame = CGRectOffset(view.frame, 0, -(scrollBgMove / riseRatio))
+		bgBlurImageView?.frame = newFrame
+		bgImageView?.frame = newFrame
 	}
 	
+	/* image manipulation routines */
+	
+	func resizeImageToNonRetina (original:UIImage) -> UIImage {
+		let newSize = CGSizeMake(view.frame.size.width, view.frame.size.height + 30.0)
+		UIGraphicsBeginImageContextWithOptions(newSize, true, 1.0)
+		original.drawInRect(CGRectMake(0, 0, view.frame.size.width, view.frame.size.height + 30.0))
+		let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+		UIGraphicsEndImageContext()
+		return resizedImage
+	}
+	
+	// second version of the blur routine
+	func applyBlurEffectToImage (original: UIImage) -> UIImage {
+		let imageToBlur = CIImage(image: original)
+		
+		let blurfilter = CIFilter(name: "CIGaussianBlur")
+		blurfilter.setValue(imageToBlur, forKey: kCIInputImageKey)
+		blurfilter.setValue(5.0, forKey: kCIInputRadiusKey)
+		
+		// get a full rect, including the hidden bits
+		let fullRect = CGRectMake(0, 0, imageToBlur.extent().width, imageToBlur.extent().height) //  + 30.0)
+		
+		let context = CIContext(options:[kCIContextUseSoftwareRenderer : true])
+		let cgimg = context.createCGImage(blurfilter.outputImage, fromRect: fullRect) // imageToBlur.extent())
+		let blurredImage = UIImage(CGImage: cgimg)
+		
+		// if we wanted to daisy chain filters:
+		// raywenderlich.com/76285/beginning-core-image-swift
+		
+		return blurredImage!
+	}
+	
+	/* this is the old version
 	func applyBlurEffectToImage (original: UIImage) -> UIImage {
 		UIGraphicsBeginImageContextWithOptions(view.bounds.size, true, 0.0)
 		var imgaa :UIImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -83,16 +131,6 @@ class MainViewController: UIViewController {
 		UIGraphicsEndImageContext()
 		
 		return finalImage
-	}
-	
-	/*
-	func applyBlurEffectToImage (original: UIImage) -> UIImage {
-		var imageToBlur = CIImage(image: original)
-		var blurfilter = CIFilter(name: "CIGaussianBlur")
-		blurfilter.setValue(imageToBlur, forKey: "inputImage")
-		var resultImage = blurfilter.valueForKey("outputImage") as CIImage
-		var blurredImage = UIImage(CIImage: resultImage)
-		return blurredImage!
 	} */
 }
 
@@ -112,7 +150,7 @@ extension MainViewController: UICollectionViewDataSource {
 		} else {
 			cell = collectionView.dequeueReusableCellWithReuseIdentifier("OtherCell", forIndexPath: indexPath) as! UICollectionViewCell
 			cell.backgroundColor = UIColor.blackColor()
-			cell.alpha = 0.5
+			cell.alpha = 0.3
 		}
 		return cell
 	}
